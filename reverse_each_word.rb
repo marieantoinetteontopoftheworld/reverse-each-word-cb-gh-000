52f050477f509043802614507c5866cba813bf08
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-03 18:14:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence_string)
   sentence_array = sentence_string.split(' ')
   sentence_array.map do |word_string|
-    word_string.split.reverse
+    word_string.split('').reverse.join('')
   end
+  sentence_array.join('')
 end
